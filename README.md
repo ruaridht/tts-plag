420cdc0757aaf719f515b216a336b0b098f85bc1
Ruaridh Thomson <echelous@me.com>
2011-11-06 12:46:16 +0000

    Initial structure

---
+++ README.md
@@ -0,0 +1,5 @@
+# TTS Practical - Plagiarism detector
+Ruaridh Thomson
+
+## Description
+Implement a tool to detect duplicates and plagiarism among text files.
---
+++ engine.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+"""
+A simple plagiarism detector
+
+Ruaridh Thomson
+s0786036
+
+Practical 2 for Text Technologies
+"""
+
+import re
+import math
+import time
+
+def main():
+  print "Goodbye."
+
+if __name__ == "__main__":
+  main()
